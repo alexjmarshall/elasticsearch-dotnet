0e14b12e586547e2509204841ac60f3ad1e2c1f3
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-04-18 17:04:16 -0600

    add project base

---
+++ src/NuSearch.Indexer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using Nest;
+using NuSearch.Domain;
+using NuSearch.Domain.Data;
+using NuSearch.Domain.Model;
+
+namespace NuSearch.Indexer
+{
+	class Program
+	{
+		private static ElasticClient Client { get; set; }
+		private static NugetDumpReader DumpReader { get; set; }
+
+		static void Main(string[] args)
+		{
+			Client = NuSearchConfiguration.GetClient();
+			var directory = args.Length > 0 && !string.IsNullOrEmpty(args[0]) 
+				? args[0] 
+				: NuSearchConfiguration.PackagePath;
+			DumpReader = new NugetDumpReader(directory);
+
+			IndexDumps();
+
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
+
+		static void IndexDumps()
+		{
+			Console.WriteLine("Done.");
+		}
+	}
+}
