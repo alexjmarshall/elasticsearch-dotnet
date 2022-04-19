81fe57b4bbf7b44e92309782cc21a14ddc8ae943
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2022-04-19 04:52:05 -0600

    implement HttpPost Index method to query for autocomplete suggestions

--- src/NuSearch.Web/Controllers/SuggestController.cs
+++ src/NuSearch.Web/Controllers/SuggestController.cs
@@ -15,7 +15,36 @@ namespace NuSearch.Web.Controllers
         [HttpPost]
         public IActionResult Index([FromBody]SearchForm form)
         {
-            // TODO: implement
+            var result = _client.Search<Package>(s => s
+                .Index<Package>()
+                .Source(sf => sf
+                    .Includes(f => f
+                        .Field(ff => ff.Id)
+                        .Field(ff => ff.DownloadCount)
+                        .Field(ff => ff.Summary)
+                    )
+                )
+                .Suggest(su => su
+                    .Completion("package-suggestions", c => c
+                        .Prefix(form.Query)
+                        .Field(p => p.Suggest)
+                    )
+                )
+            );
+
+            var suggestions = result.Suggest["package-suggestions"]
+                .FirstOrDefault()
+                .Options
+                .Select(suggest => new
+                {
+                    id = suggest.Source.Id,
+                    downloadCount = suggest.Source.DownloadCount,
+                    summary = !string.IsNullOrEmpty(suggest.Source.Summary)
+                        ? string.Concat(suggest.Source.Summary.Take(200))
+                        : string.Empty
+                });
+
+            return Json(suggestions);
         }
     }
 }
